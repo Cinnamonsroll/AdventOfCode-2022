83052cc3ac7b104005a20dee11206c5af4d50639
MakeshiftJuliette <71993095+Cinnamonsroll@users.noreply.github.com>
2022-12-01 09:33:39 -0500

    Create solution.py

---
+++ day1/python/solution.py
@@ -0,0 +1,15 @@
+with open('./input.txt', 'r') as handle:
+    text = handle.read().splitlines()
+    i = 0
+    length = len(text)
+    a = []
+    final = []
+    while i < length:
+      line = text[i]
+      if not line:
+        final.append(sum(a))
+        a = []
+      else:
+        a.append(int(line))
+      i += 1
+    print(max(final))
