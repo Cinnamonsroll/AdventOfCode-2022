73c6e1f8a53209e1335d54843a862f6782b976a7
MakeshiftJuliette <71993095+Cinnamonsroll@users.noreply.github.com>
2022-12-01 08:22:37 -0500

    Create solution.rb

---
+++ day1/ruby/solution.rb
@@ -0,0 +1,15 @@
+file = File.open('input.txt')
+file_data = file.readlines.map(&:chomp)
+a = []
+final = []
+(0..file_data.length).each do |i|
+  line = file_data[i]
+  if line.nil? || line.empty?
+    final.push(a.sum)
+    a = []
+  else
+    a.push(line.to_i)
+  end
+end
+
+puts final.max()
