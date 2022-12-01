4199d70a18c819eda821f84049c3fc8c6107e4b9
MakeshiftJuliette <71993095+Cinnamonsroll@users.noreply.github.com>
2022-12-01 08:07:46 -0500

    Create solution.js

---
+++ day1/javascript/solution.js
@@ -0,0 +1,9 @@
+const { readFileSync } = require("node:fs");
+const input = readFileSync("./input.txt", "utf-8").split("\n");
+const final = [];
+for (let i = 0, a = []; i < input.length; i++) {
+  const line = input[i];
+  if (!line) final.push(a.reduce((a, b) => a + b, 0)), a = []
+  else a.push(+line)
+}
+console.log(Math.max(...final))
